bd93f9e7619eff39391dca34c529f7e4b19ee18b
Davi-silva <davisilvasanttos2026@gmail.com>
2026-05-11 11:05:20 -0300

    Add files via upload

---
+++ açaiteria.py
@@ -0,0 +1,59 @@
+
+print("  Bem-vindo a açaiteria Quintal do Açaí ")
+
+
+print("\n AÇAÍS TRADICIONAIS")
+print("- 300ml  | R$ 12,00")
+print("- 500ml  | R$ 18,00")
+print("- 700ml  | R$ 24,00")
+print("- 1 Litro| R$ 35,00")
+
+print("\n AÇAÍS ESPECIAIS")
+print("- Açaí Morango Mix  | R$ 22,00")
+print("- Açaí ChocoMax     | R$ 26,00")
+print("- Açaí Tropical     | R$ 24,00")
+print("- Açaí Power        | R$ 28,00")
+
+print("\n ADICIONAIS")
+
+print("\nFrutas")
+print("- Banana            | R$ 2,00")
+print("- Morango           | R$ 3,00")
+print("- Kiwi              | R$ 4,00")
+print("- Manga             | R$ 3,00")
+print("- Uva               | R$ 3,00")
+print("- Abacaxi           | R$ 3,00")
+
+print("\n Cremes")
+print("- Nutella           | R$ 5,00")
+print("- Leite Condensado  | R$ 2,50")
+print("- Creme de Ninho    | R$ 4,00")
+print("- Paçoca Cremosa    | R$ 3,50")
+print("- Creme de Amendoim | R$ 4,00")
+print("- Calda Chocolate   | R$ 3,00")
+
+print("\n Crocantes")
+print("- Granola           | R$ 2,00")
+print("- Castanha          | R$ 4,00")
+print("- Confete           | R$ 2,50")
+print("- Ovomaltine        | R$ 4,00")
+print("- Chocoball         | R$ 3,00")
+print("- Sucrilhos         | R$ 3,00")
+
+print("\n COMBOS PROMOCIONAIS")
+print("- Combo Casal       | R$ 38,00")
+print("- Combo Família     | R$ 45,00")
+print("- Combo Supremo     | R$ 32,00")
+
+print(" Obrigado pela preferência!")
+
+
+print("Preferencias de Tamanho")
+
+escolha = input("Escolha o tamanho: ")
+
+print("tamanhos:")
+
+if escolha == "1":
+
+    preco = 
